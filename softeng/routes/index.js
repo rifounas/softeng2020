e4b9be113294ac7e9be802532d53adb42cd79705
rifounas <63539343+rifounas@users.noreply.github.com>
2020-05-31 15:20:04 +0300

    Update index.js

--- softeng/routes/index.js
+++ softeng/routes/index.js
@@ -2,17 +2,37 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
-//WElcome page
+
 router.get('/', (req, res) => res.render('welcome'));
 
-//dashboard
+
 router.get('/dashboard', ensureAuthenticated, (req, res) => 
    res.render('dashboard',{
    name: req.user.name
 }));
 
-router.get('/soon', ensureAuthenticated, (req, res) =>
-   res.render('soon',{
+router.get('/rating', ensureAuthenticated, (req, res) =>
+   res.render('rating',{
+   name: req.user.name
+}));
+
+router.get('/search', ensureAuthenticated, (req, res) =>
+   res.render('search',{
+   name: req.user.name
+}));
+
+router.get('/history', ensureAuthenticated, (req, res) =>
+   res.render('history',{
+   name: req.user.name
+}));
+
+router.get('/results', ensureAuthenticated, (req, res) =>
+   res.render('results',{
+   name: req.user.name
+}));
+
+router.get('/thanks', ensureAuthenticated, (req, res) =>
+   res.render('thanks',{
    name: req.user.name
 }));
 
